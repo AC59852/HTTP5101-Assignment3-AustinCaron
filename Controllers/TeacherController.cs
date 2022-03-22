7830caa50ebc1804719fde0df8087a039b6f734f
AC59852 <austin.caron1@gmail.com>
2022-03-22 18:10:29 -0400

    Added routing and view for specific teacher based on id in the DB

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -1,32 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
 using HTTP5101_Assignment3_AustinCaron.Models;
 using MySql.Data.MySqlClient;
 
 namespace HTTP5101_Assignment3_AustinCaron.Controllers
 {
-    public class TeacherController : ApiController
+    public class TeacherController : Controller
     {
-        // The database context class which allows us to access our MySQL Database.
         private SchoolDbContext School = new SchoolDbContext();
 
-        //This Controller Will access the authors table of our blog database.
-        /// <summary>
-        /// Returns a list of Authors in the system
-        /// </summary>
-        /// <example>GET api/AuthorData/ListAuthors</example>
-        /// <returns>
-        /// A list of authors (first names and last names)
-        /// </returns>
-        [HttpGet]
-        [Route("api/TeacherData/Teacher/{id}")]
-        public IEnumerable<string> TeacherData(int id)
+        [Route("/Teacher/List")]
+
+        // GET: Teacher
+        [Route("/Teacher/Show/{id}")]
+        public ActionResult Show(int id)
         {
-            //Create an instance of a connection
+            string TeacherName,
+                   EmployeeNumber,
+                   HireDate,
+                   Salary;
+
             MySqlConnection Conn = School.AccessDatabase();
 
             //Open the connection between the web server and database
@@ -41,33 +37,23 @@ namespace HTTP5101_Assignment3_AustinCaron.Controllers
             //Gather Result Set of Query into a variable
             MySqlDataReader ResultSet = cmd.ExecuteReader();
 
-            //Create an empty list of Author Names
-            List<string> TeacherData = new List<string> { };
-
-            //Loop Through Each Row the Result Set
             while (ResultSet.Read())
             {
                 //Access Column information by the DB column name as an index
-                string TeacherName = ResultSet["teacherfname"] + " " + ResultSet["teacherlname"];
-                string EmployeeNumber = (string)ResultSet["employeenumber"];
-                string HireDate = ResultSet["hiredate"].ToString();
-                string Salary = ResultSet["salary"].ToString();
+                TeacherName = ResultSet["teacherfname"] + " " + ResultSet["teacherlname"];
+                EmployeeNumber = (string)ResultSet["employeenumber"];
+                HireDate = ResultSet["hiredate"].ToString();
+                Salary = ResultSet["salary"].ToString();
 
-                //Add the Teacher Data to the List
-                TeacherData.Add(TeacherName);
-                TeacherData.Add(EmployeeNumber);
-                TeacherData.Add(HireDate);
-                TeacherData.Add(Salary);
-
-
+                ViewData["TeacherName"] = TeacherName;
+                ViewData["EmployeeNumber"] = EmployeeNumber;
+                ViewData["HireDate"] = HireDate;
+                ViewData["Salary"] = Salary;
             }
 
-            //Close the connection between the MySQL Database and the WebServer
             Conn.Close();
 
-            //Return the final list of author names
-            return TeacherData;
+            return View();
         }
-
     }
 }
